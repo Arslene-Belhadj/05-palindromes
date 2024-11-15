a6296018b3cb41eed5ae27beb319806f6c3ac0a7
Arslene-BELHADJ <arslene.belhadj@edu.esiee.fr>
2024-11-15 14:07:47 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+"""
+Import essentiel au bon fonctonnement
+"""
+import string
+from unidecode import unidecode
 #### Fonction secondaire
 
 
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """
+    Fonction vérifiant si le mot p est un palindrome
+    """
+    p=unidecode(p).replace(" ","").lower().translate(str.maketrans('','',string.punctuation))
+    if len(p)==1:
+        return True
+    if p=="":
+        return True
+    if p[0]==p[-1]:
+        return ispalindrome(p[1:-1])
     return False
 
-#### Fonction principale
-
-
 def main():
-
+    """
+    fonction main
+    """
     # vos appels à la fonction secondaire ici
 
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie","sé - ES"]:
         print(s, ispalindrome(s))
 
 
